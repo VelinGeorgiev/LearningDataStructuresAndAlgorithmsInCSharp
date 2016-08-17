f586c54af17275179833ccb8ae7799d16bb3bd2b
Velin <velin.georgiev@gmail.com>
2016-08-18 00:11:13 +0300

    Graph added

---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructures/Graph.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+
+namespace Learning.DataStructures
+{
+    /// <summary>
+    /// Adapted from source: http://www.introprogramming.info/english-intro-csharp-book/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Graph<T>
+    {
+        internal const int MaxNode = 1024;
+        private readonly int[][] _childNodes;
+
+        public Graph(int[][] childNodes)
+        {
+            _childNodes = childNodes;
+        }
+
+        public void BreadthFirstSearchTraversal(int node, IList<int> result)
+        {
+            bool[] visited = new bool[MaxNode];
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(node);
+            visited[node] = true;
+            while (queue.Count > 0)
+            {
+                int currentNode = queue.Dequeue();
+                result.Add(currentNode);
+
+                foreach (int childNode in _childNodes[currentNode])
+                {
+                    if (!visited[childNode])
+                    {
+                        queue.Enqueue(childNode);
+                        visited[childNode] = true;
+                    }
+                }
+            }
+        }
+
+        public void DepthFirstSearchTraversalWithStack(int node, IList<int> result)
+        {
+            bool[] visited = new bool[MaxNode];
+            Stack<int> stack = new Stack<int>();
+            stack.Push(node);
+            visited[node] = true;
+            while (stack.Count > 0)
+            {
+                int currentNode = stack.Pop();
+                result.Add(currentNode);
+
+                foreach (int childNode in _childNodes[currentNode])
+                {
+                    if (!visited[childNode])
+                    {
+                        stack.Push(childNode);
+                        visited[childNode] = true;
+                    }
+                }
+            }
+        }
+
+        public void DepthFirstSearchTraversal(int node, bool[] visited, IList<int> result)
+        {
+            if (!visited[node])
+            {
+                visited[node] = true;
+                result.Add(node);
+
+                foreach (int childNode in _childNodes[node])
+                {
+                    DepthFirstSearchTraversal(childNode, visited, result);
+                }
+            }
+        }
+    }
+}
---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructuresTests/GraphTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Learning.DataStructures;
+using NUnit.Framework;
+
+namespace Learning.DataStructuresTests
+{
+    [TestFixture]
+    public class GraphTests
+    {
+        private readonly Graph<int> _graph;
+
+        public GraphTests()
+        {
+            _graph = new Graph<int>(new int[][]
+            {
+                new int[] {3, 6}, // successors of vertice 0
+                new int[] {2, 3, 4, 5, 6}, // successors of vertice 1
+                new int[] {1, 4, 5}, // successors of vertice 2
+                new int[] {0, 1, 5}, // successors of vertice 3
+                new int[] {1, 2, 6}, // successors of vertice 4
+                new int[] {1, 2, 3}, // successors of vertice 5
+                new int[] {0, 1, 4} // successors of vertice 6
+            });
+        }
+
+        [Test]
+        public void BreadthFirstSearchTraversalTest()
+        {
+            var result = new List<int>();
+            _graph.BreadthFirstSearchTraversal(0, result);
+
+            Assert.AreEqual(result[0], 0);
+            Assert.AreEqual(result[1], 3);
+            Assert.AreEqual(result[2], 6);
+            Assert.AreEqual(result[3], 1);
+            Assert.AreEqual(result[4], 5);
+            Assert.AreEqual(result[5], 4);
+            Assert.AreEqual(result[6], 2);
+        }
+
+        [Test]
+        public void DepthFirstSearchTraversalWithStackTest()
+        {
+            var result = new List<int>();
+            _graph.DepthFirstSearchTraversalWithStack(0, result);
+
+            Assert.AreEqual(result[0], 0);
+            Assert.AreEqual(result[1], 6);
+            Assert.AreEqual(result[2], 4);
+            Assert.AreEqual(result[3], 2);
+            Assert.AreEqual(result[4], 5);
+            Assert.AreEqual(result[5], 1);
+            Assert.AreEqual(result[6], 3);
+        }
+
+        [Test]
+        public void DepthFirstSearchTraversalTest()
+        {
+            var result = new List<int>();
+            var visited = new bool[100];
+            _graph.DepthFirstSearchTraversal(0, visited, result);
+
+            Assert.AreEqual(result[0], 0);
+            Assert.AreEqual(result[1], 3);
+            Assert.AreEqual(result[2], 1);
+            Assert.AreEqual(result[3], 2);
+            Assert.AreEqual(result[4], 4);
+            Assert.AreEqual(result[5], 6);
+            Assert.AreEqual(result[6], 5);
+        }
+    }
+}
