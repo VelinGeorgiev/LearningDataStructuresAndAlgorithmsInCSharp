7f4ef79b15d74738500a99a988d466aeb1fd775f
Velin <velin.georgiev@gmail.com>
2016-08-17 23:45:38 +0300

    Tree BFS, DFS sample and tests added

---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructures/Tree.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Learning.DataStructures
+{
+    /// <summary>
+    /// Adapted from source: http://www.introprogramming.info/english-intro-csharp-book/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TreeNode<T>
+    {
+        private bool _hasParent;
+        private readonly IList<TreeNode<T>> _children;
+
+        public TreeNode(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            Value = value;
+            _children = new List<TreeNode<T>>();
+        }
+
+        public T Value { get; set; }
+
+        public int ChildrenCount => _children.Count;
+
+        public void AddChild(TreeNode<T> child)
+        {
+            if (child == null)
+                throw new ArgumentNullException(nameof(child));
+            if (child._hasParent)
+                throw new ArgumentException(nameof(_hasParent));
+
+            child._hasParent = true;
+            _children.Add(child);
+        }
+
+        public TreeNode<T> GetChild(int index)
+        {
+            return _children[index];
+        }
+    }
+
+    public class Tree<T>
+    {
+        private readonly TreeNode<T> _root;
+        public TreeNode<T> Root => _root;
+
+        public Tree(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+            _root = new TreeNode<T>(value);
+        }
+
+        public Tree(T value, params Tree<T>[] children) : this(value)
+        {
+            foreach (Tree<T> child in children)
+            {
+                _root.AddChild(child._root);
+            }
+        }
+
+        public void BreadthFirstSearchTraversal(IList<T> result)
+        {
+            Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+            queue.Enqueue(_root);
+            while (queue.Count > 0)
+            {
+                TreeNode<T> currentNode = queue.Dequeue();
+                result.Add(currentNode.Value);
+
+                for (int i = 0; i < currentNode.ChildrenCount; i++)
+                {
+                    TreeNode<T> childNode = currentNode.GetChild(i);
+                    queue.Enqueue(childNode);
+                }
+            }
+        }
+
+        public void DepthFirstSearchTraversal(TreeNode<T> root, IList<T> result)
+        {
+            if (_root == null) return;
+            result.Add(root.Value);
+
+            TreeNode<T> child = null;
+            for (int i = 0; i < root.ChildrenCount; i++)
+            {
+                child = root.GetChild(i);
+                DepthFirstSearchTraversal(child, result);
+            }
+        }
+
+        public void DepthFirstSearchTraversalWithStack(IList<T> result)
+        {
+            Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+            stack.Push(_root);
+            while (stack.Count > 0)
+            {
+                TreeNode<T> currentNode = stack.Pop();
+                result.Add(currentNode.Value);
+
+                for (int i = 0; i < currentNode.ChildrenCount; i++)
+                {
+                    TreeNode<T> childNode = currentNode.GetChild(i);
+                    stack.Push(childNode);
+                }
+            }
+        }
+    }
+}
---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructuresTests/TreeTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using Learning.DataStructures;
+using NUnit.Framework;
+
+namespace Learning.DataStructuresTests
+{
+    [TestFixture]
+    public class TreeTests
+    {
+        private readonly Tree<int> _tree;
+
+        public TreeTests()
+        {
+            _tree =
+                new Tree<int>(8,
+                    new Tree<int>(20,
+                        new Tree<int>(1),
+                        new Tree<int>(12),
+                        new Tree<int>(35)),
+                    new Tree<int>(21),
+                    new Tree<int>(14,
+                        new Tree<int>(24),
+                        new Tree<int>(6))
+                );
+        }
+
+        [Test]
+        public void BreadthFirstSearchTraversalTest()
+        {
+            var result = new List<int>();
+            _tree.BreadthFirstSearchTraversal(result);
+
+            Assert.AreEqual(result[0], 8);
+            Assert.AreEqual(result[1], 20);
+            Assert.AreEqual(result[2], 21);
+            Assert.AreEqual(result[3], 14);
+            Assert.AreEqual(result[4], 1);
+            Assert.AreEqual(result[5], 12);
+            Assert.AreEqual(result[6], 35);
+            Assert.AreEqual(result[7], 24);
+            Assert.AreEqual(result[8], 6);
+        }
+
+        [Test]
+        public void DepthFirstSearchTraversalWithStackTest()
+        {
+            var result = new List<int>();
+            _tree.DepthFirstSearchTraversalWithStack(result);
+
+            Assert.AreEqual(result[0], 8);
+            Assert.AreEqual(result[1], 14);
+            Assert.AreEqual(result[2], 6);
+            Assert.AreEqual(result[3], 24);
+            Assert.AreEqual(result[4], 21);
+            Assert.AreEqual(result[5], 20);
+            Assert.AreEqual(result[6], 35);
+            Assert.AreEqual(result[7], 12);
+            Assert.AreEqual(result[8], 1);
+        }
+
+        [Test]
+        public void DepthFirstSearchTraversalTest()
+        {
+            var result = new List<int>();
+            _tree.DepthFirstSearchTraversal(_tree.Root, result);
+
+            Assert.AreEqual(result[0], 8);
+            Assert.AreEqual(result[1], 20);
+            Assert.AreEqual(result[2], 1);
+            Assert.AreEqual(result[3], 12);
+            Assert.AreEqual(result[4], 35);
+            Assert.AreEqual(result[5], 21);
+            Assert.AreEqual(result[6], 14);
+            Assert.AreEqual(result[7], 24);
+            Assert.AreEqual(result[8], 6);
+        }
+    }
+}
