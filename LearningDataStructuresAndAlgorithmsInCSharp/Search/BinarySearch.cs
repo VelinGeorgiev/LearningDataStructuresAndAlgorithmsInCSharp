944352d52d7c4766c622887272a1d93aaba4b855
Velin <velin.georgiev@gmail.com>
2016-08-17 15:47:54 +0300

    search refactoring

--- LearningDataStructuresAndAlgorithmsInCSharp/Search/BinarySearch.cs
+++ LearningDataStructuresAndAlgorithmsInCSharp/Search/BinarySearch.cs
@@ -13,15 +13,16 @@ namespace Learning.Search
             int hi = array.Length - 1;
             do
             {
-                //int mid = (min + max)/2;
                 int mid = lo + (hi - lo)/2;
+
+                if (array[mid].CompareTo(item) == 0)
+                    return array[mid];
+
                 if (item.CompareTo(array[mid]) > 0)
                     lo = mid + 1;
                 else
                     hi = mid - 1;
-                if (array[mid].CompareTo(item) == 0)
-                    return array[mid];
-                //if (lo > hi) break;
+
             } while (lo <= hi);
 
             return default(T);
