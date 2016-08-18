a429b53cf3f33020a0010ba60eb80afef21794a1
Velin <velin.georgiev@gmail.com>
2016-08-18 12:47:34 +0300

    more binary tree traversals added

---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructures/BinaryTree.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Learning.DataStructures
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left, Right;
+
+        public Node(int item)
+        {
+            Data = item;
+            Left = Right = null;
+        }
+    }
+
+    /// <summary>
+    /// BinaryTree with BSF and DFS.
+    /// Modified from source: http://www.geeksforgeeks.org/618/
+    /// </summary>
+    public class BinaryTree
+    {
+        //Root of the Binary Tree
+        public Node Root;
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public void InorderDepthFirstTraversal(Node node, IList<Node> result)
+        {
+            if (node == null) return;
+
+            // First recur on left child.
+            InorderDepthFirstTraversal(node.Left, result);
+
+            // Then print the data of node.
+            // System.out.print(node.key);
+            Console.Write(node.Data);
+            result.Add(node);
+
+            // Now recur on right child.
+            InorderDepthFirstTraversal(node.Right, result);
+        }
+
+        public void PreorderDepthFirstTraversal(Node node, IList<Node> result)
+        {
+            if (node == null) return;
+
+            // First print data of node.
+            // System.out.print(node.key);
+            Console.Write(node.Data);
+            result.Add(node);
+
+            // Then recur on left sutree.
+            PreorderDepthFirstTraversal(node.Left, result);
+
+            // Now recur on right subtree
+            PreorderDepthFirstTraversal(node.Right, result);
+        }
+
+        public void PostorderDepthFirstTraversal(Node node, IList<Node> result)
+        {
+            if (node == null) return;
+
+            // First recur on left subtree.
+            PostorderDepthFirstTraversal(node.Left, result);
+
+            // Then recur on right subtree.
+            PostorderDepthFirstTraversal(node.Right, result);
+
+            // Now deal with the node.
+            // System.out.print(node.Data);
+            Console.Write(node.Data);
+            result.Add(node);
+        }
+
+        public void BreadthFirstTraversal(Node node)
+        {
+            
+        }
+
+        public void DepthFirstTraversalWithStack()
+        {
+
+        }
+    }
+}
---
+++ LearningDataStructuresAndAlgorithmsInCSharp/DataStructuresTests/BinaryTreeTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Learning.DataStructures;
+using NUnit.Framework;
+
+namespace Learning.DataStructuresTests
+{
+    [TestFixture]
+    public class BinaryTreeTests
+    {
+        private readonly BinaryTree _tree;
+
+        public BinaryTreeTests()
+        {
+            _tree = new BinaryTree();
+            _tree.Root = new Node(1);
+            _tree.Root.Left = new Node(2);
+            _tree.Root.Right = new Node(3);
+            _tree.Root.Left.Left = new Node(4);
+            _tree.Root.Left.Right = new Node(5);
+        }
+
+        [Test]
+        public void InorderDepthFirstTraversal()
+        {
+            var result = new List<Node>();
+            _tree.InorderDepthFirstTraversal(_tree.Root, result);
+
+            Assert.AreEqual(result[0].Data, 4);
+            Assert.AreEqual(result[1].Data, 2);
+            Assert.AreEqual(result[2].Data, 5);
+            Assert.AreEqual(result[3].Data, 1);
+            Assert.AreEqual(result[4].Data, 3);
+        }
+
+        [Test]
+        public void PreorderDepthFirstTraversal()
+        {
+            var result = new List<Node>();
+            _tree.PreorderDepthFirstTraversal(_tree.Root, result);
+
+            Assert.AreEqual(result[0].Data, 1);
+            Assert.AreEqual(result[1].Data, 2);
+            Assert.AreEqual(result[2].Data, 4);
+            Assert.AreEqual(result[3].Data, 5);
+            Assert.AreEqual(result[4].Data, 3);
+        }
+
+        [Test]
+        public void PostorderDepthFirstTraversal()
+        {
+            var result = new List<Node>();
+            _tree.PostorderDepthFirstTraversal(_tree.Root, result);
+
+            Assert.AreEqual(result[0].Data, 4);
+            Assert.AreEqual(result[1].Data, 5);
+            Assert.AreEqual(result[2].Data, 2);
+            Assert.AreEqual(result[3].Data, 3);
+            Assert.AreEqual(result[4].Data, 1);
+        }
+    }
+}
