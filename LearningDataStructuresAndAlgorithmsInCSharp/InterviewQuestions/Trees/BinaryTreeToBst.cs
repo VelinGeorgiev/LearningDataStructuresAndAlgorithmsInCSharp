b704b53e64fbe25204adc5212fbf1c2bbe04ca51
Velin <velin.georgiev@gmail.com>
2016-08-19 14:14:57 +0300

    b tree to bst tree example added

---
+++ LearningDataStructuresAndAlgorithmsInCSharp/InterviewQuestions/Trees/BinaryTreeToBst.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Learning.DataStructures;
+
+namespace Learning.InterviewQuestions.Trees
+{
+    /// <summary>
+    /// http://www.geeksforgeeks.org/binary-tree-to-binary-search-tree-conversion/
+    /// </summary>
+    public class BinaryTreeToBst : BinaryTree
+    {
+        public void Convert(Node node)
+        {
+            if(node == null) return;
+
+            int count = CountNodes(node);
+            int[] array = new int[count];
+            StoreInorder(node, array);
+            Array.Sort(array); // Modified quicksort worst O(n2)
+            ArrayToBst(node, array);
+        }
+
+        private int _abIndex = 0;
+        private void ArrayToBst(Node node, int[] array)
+        {
+            if(node == null) return;
+            ArrayToBst(node.left, array);
+            node.data = array[_abIndex];
+            _abIndex++;
+            ArrayToBst(node.right, array);
+        }
+
+        private int _siIndex = 0;
+        private void StoreInorder(Node node, int[] array)
+        {
+            if(node == null) return;
+            StoreInorder(node.left, array);
+            array[_siIndex] = node.data;
+            _siIndex++;
+            StoreInorder(node.right, array);
+        }
+
+        private int CountNodes(Node node)
+        {
+            if (node == null) return 0;
+            return CountNodes(node.left) + CountNodes(node.right) + 1;
+        }
+
+        /* Utility function to print inorder traversal of Binary Tree */
+        public void PrintInorder(Node node, IList<int> list)
+        {
+            if (node == null) return;
+
+            /* first recur on left child */
+            PrintInorder(node.left, list);
+
+            /* then print the data of node */
+            Console.WriteLine(node.data);
+            list.Add(node.data);
+
+            /* now recur on right child */
+            PrintInorder(node.right, list);
+    }
+}
+}
---
+++ LearningDataStructuresAndAlgorithmsInCSharp/InterviewQuestionsTests/Trees/BinaryTreeToBstTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Learning.DataStructures;
+using Learning.InterviewQuestions.Trees;
+using NUnit.Framework;
+
+namespace Learning.InterviewQuestionsTests.Trees
+{
+    [TestFixture]
+    public class BinaryTreeToBstTests
+    {
+        [Test]
+        public void Convert()
+        {
+            BinaryTreeToBst tree = new BinaryTreeToBst();
+            var r = tree.root;
+            r = new Node(10);
+            r.left = new Node(30);
+            r.right = new Node(15);
+            r.left.left = new Node(20);
+            r.left.right = new Node(5);
+
+            tree.Convert(r);
+
+            Assert.AreEqual(r.data, 20);
+            Assert.AreEqual(r.left.data, 10);
+            Assert.AreEqual(r.left.left.data, 5);
+            Assert.AreEqual(r.left.right.data, 15);
+            Assert.AreEqual(r.right.data, 30);
+        }
+
+        [Test]
+        public void ConvertAndPrint()
+        {
+            BinaryTreeToBst tree = new BinaryTreeToBst();
+            var r = tree.root;
+            r = new Node(10);
+            r.left = new Node(30);
+            r.right = new Node(15);
+            r.left.left = new Node(20);
+            r.left.right = new Node(5);
+
+            tree.Convert(r);
+            var list = new List<int>();
+            tree.PrintInorder(r,list);
+
+            Assert.AreEqual(list[0], 5);
+            Assert.AreEqual(list[1], 10);
+            Assert.AreEqual(list[2], 15);
+            Assert.AreEqual(list[3], 20);
+            Assert.AreEqual(list[4], 30);
+        }
+    }
+}
