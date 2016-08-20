962694688d5f96a20963ffc605dc9dd6c11eab5f
Velin <velin.georgiev@gmail.com>
2016-08-20 18:35:21 +0300

    Queue as linked list

---
+++ LearningDataStructuresAndAlgorithmsInCSharp/InterviewQuestions/LinkedLists/QueueLinkedListImplementation.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Learning.InterviewQuestions.LinkedLists
+{
+    public class QNode
+    {
+        public int data { get; set; }
+        public QNode next { get; set; }
+
+        public QNode(int value)
+        {
+            data = value;
+            next = null;
+        }
+    }
+
+    public class QueueLinkedListImplementation
+    {
+        public QNode head { get; private set; }
+        public QNode tail { get; private set; }
+
+        public IList<int> list { get; set; }
+
+        public QueueLinkedListImplementation()
+        {
+            head = null;
+            tail = null;
+            list = new List<int>();
+        }
+
+        public void Enqueue(int data)
+        {
+            QNode temp = new QNode(data);
+
+            if (head == null)
+            {
+                head = temp;
+                tail = temp;
+            }
+            else
+            {
+                tail.next = temp;
+                tail = temp;
+            }
+        }
+
+        public QNode Dequeue()
+        {
+            if (head == null) return null;
+
+            QNode temp = head;
+
+            head = head.next;
+            if (head == null)  tail = null;
+
+            return temp;
+        }
+
+        public IList<int> PrintQueue()
+        {
+            QNode temp = head;
+            list = new List<int>();
+
+            while (temp != null)
+            {
+                list.Add(temp.data);
+                temp = temp.next;
+            }
+            return list;
+        }
+    }
+}
---
+++ LearningDataStructuresAndAlgorithmsInCSharp/InterviewQuestionsTests/LinkedLists/QueueLinkedListImplementationTests.cs
@@ -0,0 +1,36 @@
+﻿using Learning.DataStructures.LinkedList;
+using Learning.InterviewQuestions.LinkedLists;
+using NUnit.Framework;
+
+namespace Learning.InterviewQuestionsTests.LinkedLists
+{
+    [TestFixture]
+    public class QueueLinkedListImplementationTests
+    {
+        private readonly QueueLinkedListImplementation _queue;
+
+        public QueueLinkedListImplementationTests()
+        {
+            _queue = new QueueLinkedListImplementation();
+        }
+
+        [Test]
+        public void TestQueue()
+        {
+            _queue.Enqueue(10);
+            _queue.Enqueue(20);
+            _queue.Enqueue(30);
+            _queue.Enqueue(40);
+            _queue.Enqueue(50);
+            var list = _queue.PrintQueue();
+            Assert.AreEqual(list[0], 10);
+            Assert.AreEqual(list[4], 50);
+
+            _queue.Dequeue();
+            _queue.Dequeue();
+            var list1 = _queue.PrintQueue();
+            Assert.AreEqual(list1[0], 30);
+        }
+    }
+}
+
